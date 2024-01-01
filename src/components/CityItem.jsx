6e045e96a843d991aef87281d863eddfae4dfe56
Kennedy Musau <kennedy.musauu@gmail.com>
2024-01-01 17:42:56 +0300

    update cityList component

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,12 +1,25 @@
 // import PropTypes from "prop-types";
+import styles from "./CityItem.module.css";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
 
 function CityItem({ city }) {
-  console.log(city);
+  const { cityName, country, date, emoji, id, notes } = city;
+  console.log(cityName, country, date, emoji, id, notes);
 
   return (
-    <div>
-      <li>citt</li>
-    </div>
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>{formatDate(date)}</time>
+
+      <button className={styles.deleteBtn}>&times;</button>
+    </li>
   );
 }
 
