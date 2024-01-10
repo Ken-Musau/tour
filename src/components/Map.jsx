e7fdd58079716af784bb4e62b5282d64af80024d
Kennedy Musau <kennedy.musauu@gmail.com>
2024-01-10 22:04:41 +0300

    update map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,11 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
 
 function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   const navigate = useNavigate();
 
   const lat = searchParams.get("lat");
@@ -14,18 +17,22 @@ function Map() {
         navigate("form");
       }}
     >
-      <h1>Map</h1>
-      <h2>
-        position: {lat} and {lng}
-      </h2>
-
-      <button
-        onClick={() => {
-          setSearchParams({ lat: 50, lng: 30 });
-        }}
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={false}
+        className={styles.map}
       >
-        Change
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
